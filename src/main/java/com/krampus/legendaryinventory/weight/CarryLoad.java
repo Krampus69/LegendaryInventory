43113ffd426a2f8860154e23141cbc2930939a36
Krampus <tamereputain82@gmail.com>
2026-09-17 20:45:40 +0200

    Fixed weightless backpacks from backpacked

--- src/main/java/com/krampus/legendaryinventory/weight/CarryLoad.java
+++ src/main/java/com/krampus/legendaryinventory/weight/CarryLoad.java
@@ -1,5 +1,6 @@
 package com.krampus.legendaryinventory.weight;
 
+import com.krampus.legendaryinventory.compat.BackpackedCompat;
 import com.krampus.legendaryinventory.config.LIConfig;
 import com.krampus.legendaryinventory.inventory.LIAttachments;
 import net.minecraft.world.entity.player.Inventory;
@@ -21,6 +22,7 @@ public final class CarryLoad {
         int total = sum(inv.items) + sum(inv.armor) + sum(inv.offhand);
         total += extendedWeight(player);
         total += cursorWeight(player);
+        total += BackpackedCompat.weight(player);
         return total;
     }
 
